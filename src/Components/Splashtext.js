5a3b41a174488c99b13bdd22fe1bffb3eec1a327
zzz-Ricky <120093810+zzz-Ricky@users.noreply.github.com>
2024-08-20 19:14:05 -0600

    fixed splash scaling

---
+++ src/Components/Splashtext.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Splashtext() {
+    return (
+        <div>
+            <div className='splashBodyGrabber'>
+                click
+            </div>
+            <div className='splashBodyText' id='codeoption'>
+                me if you are interested in software
+            </div>
+            <div className='splashBodyText' id='designoption'>
+                or me if you are interested in design
+            </div>
+        </div>
+    )
+}
+
+export default Splashtext
--- src/Pages/Splash.js
+++ src/Pages/Splash.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../Styles/Simple/Splash.css'
+import '../Styles/App.css'
 import Splash1 from '../Assets/Images/Splash1.jpg'
 import Splash2 from '../Assets/Images/Splash2.jpg'
 import { Outlet, Link } from "react-router-dom";
@@ -8,17 +9,22 @@ function Splash() {
     return (
         <>
             <nav>
-                <div id='Container'>
-                    <div className='splashBodyGrabber'>
-                        click
-                    </div>
-                    <div className='splashBodyText' id='codeoption'>
-                        <li>
-                            me if you are interested in software
-                        </li>
-                    </div>
-                    <div className='splashBodyText' id='designoption'>
-                        or me if you are interested in design
+                <div id='container'>
+                    <div className='textContainer'>
+                        <div className='leftHalf'>
+                            <div className='splashBodyGrabber'>
+                                click
+                            </div>
+                            <div className='splashBodyText' id='designOption'>
+                                or me if you are interested in design.
+                            </div>
+                        </div>
+
+                        <div className='rightHalf'>
+                            <div className='splashBodyText' id='codeOption'>
+                                me if you are interested in software
+                            </div>
+                        </div>
                     </div>
                     <img className='backgroundSplash' id='backsplash1' src={Splash1} alt="[splash screen image]" />
                     <img className='backgroundSplash' id='backsplash2' src={Splash2} alt="[splash screen image]" />
